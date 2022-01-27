4b12c3426a6c3681431991a02246f5932d3236c1
Chris Hughes <Chris.Hughes1@test-and-trace.nhs.uk>
2022-01-27 18:34:37 +0000

    Consistent spacing!

--- src/main/java/uk/org/mule/jwt/internal/JwtConfiguration.java
+++ src/main/java/uk/org/mule/jwt/internal/JwtConfiguration.java
@@ -19,37 +19,37 @@ import org.mule.runtime.extension.api.annotation.param.display.Summary;
 @DisplayName("jwt-config")
 public class JwtConfiguration {
 
-  @Parameter
-  @DisplayName("Signature Algorithm")
-  @Expression(ExpressionSupport.NOT_SUPPORTED)
-  @Placement(order = 1)
-  @Summary("The encryption algorithm to be used when signing the token")
-  private JwtAlgorithm algorithm;
+    @Parameter
+    @DisplayName("Signature Algorithm")
+    @Expression(ExpressionSupport.NOT_SUPPORTED)
+    @Placement(order = 1)
+    @Summary("The encryption algorithm to be used when signing the token")
+    private JwtAlgorithm algorithm;
 
-  @Parameter
-  @Optional
-  @DisplayName("Private Key File Path")
-  @Summary("Location of the private key file")
-  @Example("src/main/resources/certs/private-key.pem")
-  @Placement(order = 2)
-  private String privateKeyPath;
+    @Parameter
+    @Optional
+    @DisplayName("Private Key File Path")
+    @Summary("Location of the private key file")
+    @Example("src/main/resources/certs/private-key.pem")
+    @Placement(order = 2)
+    private String privateKeyPath;
 
-  @Parameter
-  @Optional
-  @DisplayName("Public Key File Path")
-  @Summary("Location of the public key file")
-  @Example("src/main/resources/certs/public-key.pem")
-  @Placement(order = 3)
-  private String publicKeyPath;
+    @Parameter
+    @Optional
+    @DisplayName("Public Key File Path")
+    @Summary("Location of the public key file")
+    @Example("src/main/resources/certs/public-key.pem")
+    @Placement(order = 3)
+    private String publicKeyPath;
 
-  public SignatureAlgorithm getAlgorithm() { return algorithm.getAlgorithm(); }
-  public String getFamily() { return algorithm.getFamily(); }
+    public SignatureAlgorithm getAlgorithm() { return algorithm.getAlgorithm(); }
+    public String getFamily() { return algorithm.getFamily(); }
 
-  public String getPrivateKeyPath(){
-    return privateKeyPath;
-  }
+    public String getPrivateKeyPath(){
+        return privateKeyPath;
+    }
 
-  public String getPublicKeyPath(){
-    return publicKeyPath;
-  }
+    public String getPublicKeyPath(){
+        return publicKeyPath;
+    }
 }
--- src/main/java/uk/org/mule/jwt/internal/JwtOperations.java
+++ src/main/java/uk/org/mule/jwt/internal/JwtOperations.java
@@ -24,55 +24,55 @@ import static org.mule.runtime.extension.api.annotation.param.MediaType.APPLICAT
  * This class is a container for operations, every public method in this class will be taken as an extension operation.
  */
 public class JwtOperations {
-  /**
-   * Example of an operation that uses the configuration and a connection instance to perform some action.
-   */
-  @MediaType(value = APPLICATION_JSON, strict = true)
-  public String sign(@Content(primary = true) Map<String, Object> payload, @Config JwtConfiguration config) {
-    String jws = null;
-    try {
-      Claims claims = Jwts.claims(payload);
-      String pkcs8Key = read(config.getPrivateKeyPath());
-      PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(Base64.getDecoder().decode(pkcs8Key));
-      KeyFactory keyFactory = KeyFactory.getInstance(config.getFamily());
-      PrivateKey privateKey = keyFactory.generatePrivate(keySpec);
-      jws = Jwts.builder().setClaims(claims).signWith(privateKey, config.getAlgorithm()).compact();
+    /**
+     * Example of an operation that uses the configuration and a connection instance to perform some action.
+     */
+    @MediaType(value = APPLICATION_JSON, strict = true)
+    public String sign(@Content(primary = true) Map<String, Object> payload, @Config JwtConfiguration config) {
+        String jws = null;
+        try {
+            Claims claims = Jwts.claims(payload);
+            String pkcs8Key = read(config.getPrivateKeyPath());
+            PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(Base64.getDecoder().decode(pkcs8Key));
+            KeyFactory keyFactory = KeyFactory.getInstance(config.getFamily());
+            PrivateKey privateKey = keyFactory.generatePrivate(keySpec);
+            jws = Jwts.builder().setClaims(claims).signWith(privateKey, config.getAlgorithm()).compact();
+        }
+        catch (FileNotFoundException fnfe) {
+        }
+        catch (IOException ioe) {
+        }
+        catch (InvalidKeySpecException ikse) {
+        }
+        catch (NoSuchAlgorithmException nsae) {
+        }
+        return jws;
     }
-    catch (FileNotFoundException fnfe) {
-    }
-    catch (IOException ioe) {
-    }
-    catch (InvalidKeySpecException ikse) {
-    }
-    catch (NoSuchAlgorithmException nsae) {
-    }
-    return jws;
-  }
 
-  private static String read(final String filePath) throws FileNotFoundException, IOException {
-    BufferedReader reader = new BufferedReader(new FileReader(filePath));
-    StringBuilder builder = new StringBuilder();
-    String line = null;
-    String lineSeparator = System.getProperty("line.separator");
-    while ((line = reader.readLine()) != null) {
-      builder.append(line);
-      builder.append(lineSeparator);
+    private static String read(final String filePath) throws FileNotFoundException, IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(filePath));
+        StringBuilder builder = new StringBuilder();
+        String line = null;
+        String lineSeparator = System.getProperty("line.separator");
+        while ((line = reader.readLine()) != null) {
+            builder.append(line);
+            builder.append(lineSeparator);
+        }
+        reader.close();
+        return removeHeaders(builder.toString());
     }
-    reader.close();
-    return removeHeaders(builder.toString());
-  }
 
-  private static String removeHeaders(final String keyContents) throws IllegalArgumentException {
-    if (keyContents != null) {
-      return keyContents.replace("-----BEGIN PRIVATE KEY-----", "").
-                         replace("-----END PRIVATE KEY-----", "").
-                         replace("-----BEGIN PUBLIC KEY-----", "").
-                         replace("-----END PUBLIC KEY-----", "").
-                         replace("\r", "").
-                         replace("\n", "");
+    private static String removeHeaders(final String keyContents) throws IllegalArgumentException {
+        if (keyContents != null) {
+            return keyContents.replace("-----BEGIN PRIVATE KEY-----", "").
+                    replace("-----END PRIVATE KEY-----", "").
+                    replace("-----BEGIN PUBLIC KEY-----", "").
+                    replace("-----END PUBLIC KEY-----", "").
+                    replace("\r", "").
+                    replace("\n", "");
+        }
+        else {
+            throw new IllegalArgumentException();
+        }
     }
-    else {
-      throw new IllegalArgumentException();
-    }
-  }
 }

2e6e7efe3cf08ee42084d4f725251a4e5e86100d
Chris Hughes <Chris.Hughes1@test-and-trace.nhs.uk>
2022-02-04 11:31:58 +0000

    Define recommended subset of algorithms

--- src/main/java/uk/org/mule/jwt/internal/JwtAlgorithm.java
+++ src/main/java/uk/org/mule/jwt/internal/JwtAlgorithm.java
@@ -3,12 +3,12 @@ package uk.org.mule.jwt.internal;
 import io.jsonwebtoken.SignatureAlgorithm;
 
 enum JwtAlgorithm {
-    HS256(SignatureAlgorithm.HS256),
-    HS384(SignatureAlgorithm.HS384),
-    HS512(SignatureAlgorithm.HS512),
     ES256(SignatureAlgorithm.ES256),
     ES384(SignatureAlgorithm.ES384),
     ES512(SignatureAlgorithm.ES512),
+    PS256(SignatureAlgorithm.PS256),
+    PS384(SignatureAlgorithm.PS384),
+    PS512(SignatureAlgorithm.PS512),
     RS256(SignatureAlgorithm.RS256),
     RS384(SignatureAlgorithm.RS384),
     RS512(SignatureAlgorithm.RS512);
@@ -19,7 +19,7 @@ enum JwtAlgorithm {
         return algorithm;
     }
 
-    JwtAlgorithm(SignatureAlgorithm algorithm) {
+    JwtAlgorithm(final SignatureAlgorithm algorithm) {
         this.algorithm = algorithm;
     }
 }

cdef90cddb9cbe2834e815bfe3e7e2e94b47f5c3
Chris Hughes <Chris.Hughes1@test-and-trace.nhs.uk>
2022-02-04 16:12:52 +0000

    Use bouncycastle for PEM reading logic

--- src/main/java/uk/org/mule/jwt/internal/JwtOperations.java
+++ src/main/java/uk/org/mule/jwt/internal/JwtOperations.java
@@ -3,7 +3,10 @@ package uk.org.mule.jwt.internal;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.InvalidKeyException;
+import org.bouncycastle.asn1.pkcs.PrivateKeyInfo;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.bouncycastle.openssl.PEMParser;
+import org.bouncycastle.openssl.jcajce.JcaPEMKeyConverter;
 import org.mule.runtime.extension.api.annotation.error.Throws;
 import org.mule.runtime.extension.api.annotation.param.Config;
 import org.mule.runtime.extension.api.annotation.param.Content;
@@ -12,14 +15,11 @@ import org.mule.runtime.extension.api.annotation.param.Optional;
 import org.mule.runtime.extension.api.annotation.param.display.DisplayName;
 import org.mule.runtime.extension.api.exception.ModuleException;
 
-import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.security.*;
-import java.security.spec.InvalidKeySpecException;
-import java.security.spec.PKCS8EncodedKeySpec;
-import java.util.Base64;
+import java.security.PrivateKey;
+import java.security.Provider;
 import java.util.Map;
 
 import static org.mule.runtime.extension.api.annotation.param.MediaType.TEXT_PLAIN;
@@ -38,20 +38,23 @@ public class JwtOperations {
     public String sign(@Optional @Content Map<String, Object> header,
                        @Content(primary = true) Map<String, Object> payload,
                        @Config JwtConfiguration config) {
-        String jws;
+        String jws = null;
         try {
             Claims bodyClaims = Jwts.claims(payload);
-            String pkcs8Key = read(config.getKeyPath());
-            PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(Base64.getDecoder().decode(pkcs8Key));
-            Security.addProvider(bcProvider);
-            KeyFactory keyFactory = KeyFactory.getInstance(config.getAlgorithm().getFamilyName());
-            PrivateKey privateKey = keyFactory.generatePrivate(keySpec);
-            if (header != null) {
-                Claims headerClaims = Jwts.claims(header);
-                jws = Jwts.builder().setHeader(headerClaims).setClaims(bodyClaims).signWith(privateKey, config.getAlgorithm()).compact();
+            PEMParser parser = new PEMParser(new FileReader(config.getKeyPath()));
+            Object object = parser.readObject();
+            if (object instanceof PrivateKeyInfo) {
+                JcaPEMKeyConverter converter = new JcaPEMKeyConverter();
+                PrivateKey privateKey = converter.getPrivateKey((PrivateKeyInfo) object);
+                if (header != null) {
+                    Claims headerClaims = Jwts.claims(header);
+                    jws = Jwts.builder().setHeader(headerClaims).setClaims(bodyClaims).signWith(privateKey, config.getAlgorithm()).compact();
+                } else {
+                    jws = Jwts.builder().setClaims(bodyClaims).signWith(privateKey, config.getAlgorithm()).compact();
+                }
             }
             else {
-                jws = Jwts.builder().setClaims(bodyClaims).signWith(privateKey, config.getAlgorithm()).compact();
+                throw new InvalidKeyException(config.getKeyPath() + " identified as " + object.getClass());
             }
         }
         catch (FileNotFoundException fnfe) {
@@ -60,34 +63,9 @@ public class JwtOperations {
         catch (IOException ioe) {
             throw new ModuleException(JwtError.IO_ERROR, ioe);
         }
-        catch (InvalidKeyException | InvalidKeySpecException | NoSuchAlgorithmException ke) {
+        catch (InvalidKeyException ke) {
             throw new ModuleException(JwtError.INVALID_KEY, ke);
         }
         return jws;
-    }
-
-    private static String read(final String filePath) throws IOException {
-        BufferedReader reader = new BufferedReader(new FileReader(filePath));
-        StringBuilder builder = new StringBuilder();
-        String line;
-        String lineSeparator = System.getProperty("line.separator");
-        while ((line = reader.readLine()) != null) {
-            builder.append(line);
-            builder.append(lineSeparator);
-        }
-        reader.close();
-        return removeHeaders(builder.toString());
-    }
-
-    private static String removeHeaders(final String keyContents) throws IllegalArgumentException {
-        if (keyContents != null) {
-            return keyContents.replace("-----BEGIN PRIVATE KEY-----", "").
-                    replace("-----END PRIVATE KEY-----", "").
-                    replace("\r", "").
-                    replace("\n", "");
-        }
-        else {
-            throw new IllegalArgumentException();
-        }
     }
 }

47bb051e620302054a6064d43a2719aeca1837e1
Chris Hughes <Chris.Hughes1@test-and-trace.nhs.uk>
2022-02-04 11:32:34 +0000

    Update JwtConfiguration.java

--- src/main/java/uk/org/mule/jwt/internal/JwtConfiguration.java
+++ src/main/java/uk/org/mule/jwt/internal/JwtConfiguration.java
@@ -4,7 +4,6 @@ import io.jsonwebtoken.SignatureAlgorithm;
 import org.mule.runtime.api.meta.ExpressionSupport;
 import org.mule.runtime.extension.api.annotation.Expression;
 import org.mule.runtime.extension.api.annotation.Operations;
-import org.mule.runtime.extension.api.annotation.param.Optional;
 import org.mule.runtime.extension.api.annotation.param.Parameter;
 import org.mule.runtime.extension.api.annotation.param.display.DisplayName;
 import org.mule.runtime.extension.api.annotation.param.display.Example;
@@ -16,28 +15,27 @@ import org.mule.runtime.extension.api.annotation.param.display.Summary;
  * operations since they represent something core from the extension.
  */
 @Operations(JwtOperations.class)
-@DisplayName("jwt-config")
+@DisplayName("JWT Configuration")
 public class JwtConfiguration {
 
     @DisplayName("Signature Algorithm")
     @Expression(ExpressionSupport.NOT_SUPPORTED)
     @Parameter
     @Placement(order = 1)
-    @Summary("The encryption algorithm to be used when signing/verifying the token")
+    @Summary("The encryption algorithm to be used when signing the token")
     private JwtAlgorithm algorithm;
 
     @DisplayName("PKCS#8 Key File")
-    @Example("${mule.home}/apps/${app.name}/certificates/private-key.pem")
+    @Example("${mule.home}/apps/${app.name}/certs/pkcs8-private-key.pem")
     @Expression(ExpressionSupport.SUPPORTED)
-    @Optional
     @Parameter
     @Placement(order = 2)
     @Summary("Hint: Use ${mule.home} and ${app.name} to avoid hardcoding")
-    private String privateKeyPath;
+    private String keyPath;
 
     public SignatureAlgorithm getAlgorithm() { return algorithm.getAlgorithm(); }
 
-    public String getPrivateKeyPath(){
-        return privateKeyPath;
+    public String getKeyPath(){
+        return keyPath;
     }
 }

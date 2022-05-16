9058809c3577f9e634f4b19bc32584eaf517daa2
Chris Hughes <cajhughes@gmail.com>
2022-05-16 14:20:47 +0100

    Update JwtOperations.java

--- src/main/java/uk/org/mule/jwt/internal/JwtOperations.java
+++ src/main/java/uk/org/mule/jwt/internal/JwtOperations.java
@@ -90,9 +90,9 @@ public class JwtOperations {
         return jws;
     }
 
-    private String getJWS(@Optional @Content Map<String, Object> header,
-                          @Content(primary = true) Map<String, Object> payload,
-                          @Config JwtConfiguration config,
+    private String getJWS(Map<String, Object> header,
+                          Map<String, Object> payload,
+                          JwtConfiguration config,
                           PrivateKeyInfo privateKeyInfo) throws PEMException {
         String jws = null;
         if (privateKeyInfo != null) {
